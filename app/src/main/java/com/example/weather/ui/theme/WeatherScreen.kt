0e044af16951348a1f6bdb16c7afafc80bfd98c8
username <username@example.com>
2024-11-04 11:26:37 +0000

    location title size

--- app/src/main/java/com/example/weather/ui/theme/WeatherScreen.kt
+++ app/src/main/java/com/example/weather/ui/theme/WeatherScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.weather.R
 
 @Composable
@@ -59,8 +60,24 @@ fun WeatherSearchBar() {
 @Composable
 fun WeatherLocationTitle() {
     Row {
-        Text("Turin")
-        Text("32")
+        Column(
+            modifier = Modifier
+                .padding(6.dp)
+        ) {
+            Text(
+                text = "Turin",
+                fontSize = 32.sp
+            )
+        }
+        Column(
+            modifier = Modifier
+                .padding(6.dp)
+        ) {
+            Text(
+                text = "32°",
+                fontSize = 32.sp
+            )
+        }
     }
 }
 
@@ -91,6 +108,11 @@ fun WeatherDataCard(dataTitle: String, dataText: String, modifier: Modifier) {
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun WeatherLocationTitlePreview() {
+    WeatherLocationTitle()
+}
 
 @Preview(showBackground = true)
 @Composable
